5da5e6012d54f3511639b572804afaf88356e3c4
Kamiliya <kamiliyaprasmaisya@gmail.com>
2025-11-24 11:17:57 +0700

    try fix to deploy v3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Home CSS -->
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/home.css') }}"> -->
+    <link rel="stylesheet" href="{{ secure_asset('css/home.css') }}">
+
 
     <!-- NAVBAR CUSTOM CSS -->
 
